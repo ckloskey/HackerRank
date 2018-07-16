84b6b1a0500540820ee0e84bf6606bc6bf8dce81
ckloskey <ckloskey@gmail.com>
2018-07-16 09:37:34 -0500

    Game of Stones problem completed

---
+++ Game of Stones/Game of Stones/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    //https://www.hackerrank.com/challenges/game-of-stones-1/problem
+    static string gameOfStones(int n)
+    {
+        string winner = "";
+        return (n % 7) < 2 ? winner = "Second" : winner = "First";
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string result = gameOfStones(n);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
