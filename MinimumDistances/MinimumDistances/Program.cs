76bc0f039eb86b91cdf558d4515099239aef002d
ckloskey <ckloskey@gmail.com>
2018-10-08 18:52:30 -0500

    Minimum Distances

---
+++ MinimumDistances/MinimumDistances/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    //https://www.hackerrank.com/challenges/minimum-distances/problem
+    // Complete the minimumDistances function below.
+    static int minimumDistances(int[] a)
+    {
+        int minDistance = int.MaxValue;
+        for (int i = 0; i < a.Length; i++)
+        {
+            for (int j = i + 1; j < a.Length; j++)
+            {
+                if (a[i] == a[j])
+                {
+                    if (j - i < minDistance)
+                    {
+                        minDistance = j - i;
+                    }
+                    break;
+                }
+            }
+
+        }
+        return minDistance = minDistance == int.MaxValue ? -1 : minDistance;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+        ;
+        int result = minimumDistances(a);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
