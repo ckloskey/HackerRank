74d0b5aac1c5ac3435befd0a6a1f301233888a28
ckloskey <ckloskey@gmail.com>
2018-10-08 19:22:34 -0500

    Circular Array Roation

---
+++ CircularArrayRotation/CircularArrayRotation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System;
+
+class Solution
+{
+    //https://www.hackerrank.com/challenges/circular-array-rotation/problem
+    // Complete the circularArrayRotation function below.
+    static int[] circularArrayRotation(int[] a, int k, int[] queries)
+    {
+        int[] results = new int[queries.Length];
+        for (int i = 0; i < queries.Length; i++)
+        {
+            results[i] = a[(a.Length - (k % a.Length) + queries[i]) % a.Length];
+        }
+        return results;
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nkq = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nkq[0]);
+
+        int k = Convert.ToInt32(nkq[1]);
+
+        int q = Convert.ToInt32(nkq[2]);
+
+        int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+        ;
+
+        int[] queries = new int[q];
+
+        for (int i = 0; i < q; i++)
+        {
+            int queriesItem = Convert.ToInt32(Console.ReadLine());
+            queries[i] = queriesItem;
+        }
+
+        int[] result = circularArrayRotation(a, k, queries);
+
+        textWriter.WriteLine(string.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
